84c0cb5364d4cc01fad2396e3a19d481d83efcce
Le Minh Kha <lmkha0201@gmail.com>
2025-01-08 22:55:35 +0700

    first commit

---
+++ src/main/java/com/example/minichat/MinichatApplication.java
@@ -0,0 +1,13 @@
+package com.example.minichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinichatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinichatApplication.class, args);
+	}
+
+}

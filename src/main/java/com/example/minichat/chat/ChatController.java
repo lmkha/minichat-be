ef1e4b2e6af90a03bf6b8d7963fcecb4dd504ce2
Le Minh Kha <lmkha0201@gmail.com>
2025-01-13 23:07:26 +0700

    feat: setup original logic

---
+++ src/main/java/com/example/minichat/chat/ChatController.java
@@ -0,0 +1,39 @@
+package com.example.minichat.chat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatController {
+    private final SimpMessagingTemplate messagingTemplate;
+    private final ChatMessageService chatMessageService;
+
+    @MessageMapping("/chat")
+    public void processMessage(@Payload ChatMessage chatMessage) {
+        ChatMessage savedMsg = chatMessageService.save(chatMessage);
+        ChatNotification chatNotification = ChatNotification.builder()
+                .id(savedMsg.getId())
+                .senderId(chatMessage.getSenderId())
+                .recipientId(chatMessage.getRecipientId())
+                .content(chatMessage.getContent())
+                .build();
+        messagingTemplate.convertAndSendToUser(
+                chatMessage.getRecipientId(), "/queue/messages",
+                chatNotification
+        );
+    }
+
+    @GetMapping("/messages/{senderId}/{recipientId}")
+    public ResponseEntity<List<ChatMessage>> findChatMessages(@PathVariable String senderId, @PathVariable String recipientId) {
+        return ResponseEntity.ok(chatMessageService.findChatMessages(senderId, recipientId));
+    }
+}
---
+++ src/main/java/com/example/minichat/user/Status.java
@@ -0,0 +1,6 @@
+package com.example.minichat.user;
+
+public enum Status {
+    ONLINE,
+    OFFLINE,
+}
